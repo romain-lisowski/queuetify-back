9314e6f303b79391dc044f5340aa33c8ea825bac
Romain Lisowski <romain.lisowski@gmail.com>
2020-06-11 01:58:44 +0200

    Change port

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   }
 
   // run server
-  await app.listen(8080, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
   console.log(`Running server on: ${await app.getUrl()}`);
 }
 
