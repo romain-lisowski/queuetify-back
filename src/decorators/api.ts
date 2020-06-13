77d08eb8da650143e0a5605d72c2370e8366aac0
Romain Lisowski <romain.lisowski@gmail.com>
2020-06-13 23:40:20 +0200

    Fix decorator

--- src/decorators/api.ts
+++ src/decorators/api.ts
@@ -3,6 +3,7 @@ import { applyDecorators, Header } from '@nestjs/common';
 export function ApiHeaders() {
   return applyDecorators(
     Header('Access-Control-Allow-Origin', '*'),
+    Header('Content-type', 'application/json'),
     Header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
