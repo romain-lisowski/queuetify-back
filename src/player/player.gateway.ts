7bed6b3d3afe6e8a645732b4fb83799bbba30ccc
Romain Lisowski <romain.lisowski@gmail.com>
2020-06-15 18:08:22 +0200

    Debug traces sockets

--- src/player/player.gateway.ts
+++ src/player/player.gateway.ts
@@ -11,20 +11,24 @@ export class PlayerGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
 
   async handleConnection() {
+    console.log("IO: Connection");
     this.server.emit('user connection');
   }
 
   async handleDisconnect() {
+    console.log("IO: Disconnect");
     this.server.emit('users disconnection');
   }
 
   @SubscribeMessage('LEAVE')
   async onLeave(client, roomName) {
+    console.log("IO: Leave");
     client.leave(roomName);
   }
 
   @SubscribeMessage('JOIN')
-  async onJoin(client, roomName) {
-    client.join(roomName);
+  async onJoin(client, roomId) {
+    console.log("IO: Join " + roomId);
+    client.join(roomId);
   }
 }
