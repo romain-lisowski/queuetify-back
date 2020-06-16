d0d0d8ccb8ccfa6f91f0f148150079da97faaa81
Romain Lisowski <romain.lisowski@gmail.com>
2020-06-16 03:36:04 +0200

    Add messages

--- src/player/player.gateway.ts
+++ src/player/player.gateway.ts
@@ -5,30 +5,40 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import * as ent from 'ent';
 
 @WebSocketGateway()
 export class PlayerGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
 
   async handleConnection() {
-    console.log("IO: Connection");
+    console.log('IO: Connection');
     this.server.emit('user connection');
   }
 
   async handleDisconnect() {
-    console.log("IO: Disconnect");
+    console.log('IO: Disconnect');
     this.server.emit('users disconnection');
   }
 
   @SubscribeMessage('LEAVE')
   async onLeave(client, roomName) {
-    console.log("IO: Leave");
+    console.log('IO: Leave');
     client.leave(roomName);
   }
 
   @SubscribeMessage('JOIN')
   async onJoin(client, roomId) {
-    console.log("IO: Join " + roomId);
+    console.log('IO: Join ' + roomId);
     client.join(roomId);
+  }
+
+  @SubscribeMessage('MESSAGE')
+  async onMessage(client, data) {
+    const message = '<b>[' + data.userName + ']</b>' + ent.encode(data.message);
+    console.log('IO: Message : (' + data.roomId + ') [' + data.userName +'] ' + message);
+    this.server
+      .to(data.roomId)
+      .emit('MESSAGE', message);
   }
 }
